a6fdbc8c166de0d1cbc03117f69ef238c6aca414
nlsdizon <nlsdizon@gmail.com>
2020-05-25 23:00:12 +0800

    bible app initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://beta.ourmanna.com/api/v1/get/?format=json&order=random')
+    .then( res => res.json())
+    .then( json => {
+        this.setState({
+          isLoaded: true,
+          item: json,
+        })
+    });
+  }
+
+  render() 
+  {
+    var { isLoaded, item } = this.state;
+
+    if(!isLoaded)
+    {
+      return(
+        <div className="a"> loading . . . </div>
+      );
+    }
+    else
+    {
+      return(
+        <div>
+          <h1>VERSE OF THE DAY</h1>
+          
+          <div>
+            <p>{item.verse.details.text}</p>
+          </div>
+          <div>
+            - {item.verse.details.reference}
+          </div>
+          <div>
+            <textarea name="" id="" cols="30" rows="10">how are you feeling today after reading this verse? enter it here and click save button to save into your own database</textarea>
+          </div>
+          <div>
+            <button>SAVE</button>
+          </div>
+        </div>
+        
+      );
+    }
+  }
+
+
+  
 }
-
 export default App;
